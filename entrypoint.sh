f7abb112e5610d2bd3619918d377474d41fb0944
凨 <87279659+windfgg@users.noreply.github.com>
2023-11-18 12:51:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env sh
 
-set -e
+chmod 777 /PorxyPool
 
-if [ -z "${INPUT_USERNAME}" ]; then
-  echo "Username is empty. Please set with.username to login to docker registry."
-fi
-
-if [ -z "${INPUT_PASSWORD}" ]; then
-  echo "Password is empty. Please set with.password to login to docker registry."
-fi
-
-echo "${INPUT_PASSWORD}" | docker login -u ${INPUT_USERNAME} --password-stdin ${INPUT_REGISTRY}
-
-/publisher
+/PorxyPool
